0047006fb20550898244c5b49d7c8dcac0ba0b04
Evan Liu <hmisty@gmail.com>
2013-03-25 22:22:55 +0800

    first ci

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject followup "0.1.0-SNAPSHOT"
+  :description "followup: a new concept of todo"
+  :url "http://github.com/hmisty/followup"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :main followup.core)
